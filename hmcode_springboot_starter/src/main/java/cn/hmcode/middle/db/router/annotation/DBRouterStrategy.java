4adb95e877efe8b32a6bd738db51994cfa149a5b
hanmengjs <hanmengjs@dangdang.com>
2023-05-15 10:21:18 +0800

    添加路由策略分段标记注解

--- hmcode_springboot_starter/src/main/java/cn/hmcode/middle/db/router/annotation/DBRouterStrategy.java
+++ hmcode_springboot_starter/src/main/java/cn/hmcode/middle/db/router/annotation/DBRouterStrategy.java
@@ -12,7 +12,6 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.METHOD})
 public @interface DBRouterStrategy {
-
     boolean splitTable() default false;
 
 }
